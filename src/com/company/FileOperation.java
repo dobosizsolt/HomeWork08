429e322336a48ea675d31ffaf4fa02bc1c030e1a
dobosizsolt <dobosi.zsolt@gmail.com>
2017-12-28 15:00:12 +0100

    #2

--- src/com/company/FileOperation.java
+++ src/com/company/FileOperation.java
@@ -25,4 +25,30 @@ public class FileOperation {
             }
         }
     }
+
+    public static void fileMaker(String path, int fileQuantity, String fileName) {
+
+        String[] splFileName = new String[2];
+        splFileName = fileName.split("\\.");
+        FileOutputStream fout = null;
+
+        try {
+            for (int i = 1; i <= fileQuantity; i++) {
+                String modifiedFileName = splFileName[0] + i + "." + splFileName[1];
+                fout = new FileOutputStream(path + modifiedFileName);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (fout != null) {
+                try {
+                    fout.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         // 1. feladat ellenőrzés
-        FileOperation.fileMaker("D:\\", "proba.txt");
+       // FileOperation.fileMaker("D:\\", "proba.txt");
+
+        // 2. feladat ellenőrzés
+        FileOperation.fileMaker("D:\\", 3, "proba.txt");
     }
 }
